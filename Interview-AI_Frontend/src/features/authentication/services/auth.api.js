1c21300b1698445e779b0eb1b9ea3411dcb7e240
Jigar Vaghela <jigarvaghela2402@gmail.com>
2026-05-15 20:46:39 +0530

    updated API URLs for Vite deployment

--- Interview-AI_Frontend/src/features/authentication/services/auth.api.js
+++ Interview-AI_Frontend/src/features/authentication/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useNavigate } from "react-router";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: `${import.meta.env.VITE_API_URL}`,
     withCredentials: true,
 })
 
