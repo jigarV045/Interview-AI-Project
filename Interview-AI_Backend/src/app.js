6e167a11b1f43129fd1cbcb7a5de3209881270ce
Jigar Vaghela <jigarvaghela2402@gmail.com>
2026-05-15 20:45:53 +0530

    updated CORS to use env variable

--- Interview-AI_Backend/src/app.js
+++ Interview-AI_Backend/src/app.js
@@ -8,7 +8,7 @@ const interviewRoutes = require("../src/routes/interview.routes");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
 }));
 app.use("/api/auth", authRoutes);
