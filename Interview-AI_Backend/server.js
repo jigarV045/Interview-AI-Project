d58d21ad5f00c456d07a7ba430f23472f4ac0186
Jigar Vaghela <jigarvaghela2402@gmail.com>
2026-05-15 20:19:47 +0530

    prepare backend for deployment

--- Interview-AI_Backend/server.js
+++ Interview-AI_Backend/server.js
@@ -4,6 +4,8 @@ const connectDB = require("./src/config/db");
 
 connectDB();
 
-app.listen(3000, () => {
-    console.log('Server Running on PORT 3000');
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
